ec99588098b39432d03ac382a4b1835951be9e0a
Alex vijay raj Amalaraj <alexvijayraj@gmail.com>
2018-07-23 18:14:26 +0530

    canvas code cleanup

--- app/src/main/java/com/alexvijayraj/alextestapp/canvasActivity/HomeOverlay.java
+++ app/src/main/java/com/alexvijayraj/alextestapp/canvasActivity/HomeOverlay.java
@@ -5,6 +5,9 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.RadialGradient;
+import android.graphics.Region;
+import android.graphics.Shader;
 import android.os.Build;
 
 public class HomeOverlay {
@@ -12,40 +15,59 @@ public class HomeOverlay {
     public static Bitmap getBitMap(int width, int height){
 
         Bitmap bitMap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-
         bitMap = bitMap.copy(bitMap.getConfig(), true);
-        // Construct a canvas with the specified bitmap to draw into
+
         Canvas canvas = new Canvas(bitMap);
-        // Create a new paint with default settings.
-        Paint paint = new Paint();
-        // smooths out the edges of what is being drawn
-        paint.setAntiAlias(true);
-        // set color
-        paint.setColor(Color.WHITE);
-        // set style
-        paint.setStyle(Paint.Style.STROKE);
-        // set stroke
-        paint.setStrokeWidth(4.5f);
 
-        canvas.drawColor(0xdd007600);
+        drawCircle(canvas, width, height);
 
-//        canvas.drawCircle(width/2, height/2, width/3, paint);
+        clipCircle(canvas, width, height);
+
+        drawShadowCircle(canvas, width, height);
+
+        drawBackground(canvas);
+
+        return bitMap;
+
+    }
+
+    private static void clipCircle(Canvas canvas, int width, int height) {
 
         Path path = new Path();
 
-        path.addCircle(width/2, height/2, width/3, Path.Direction.CCW);
+        path.addCircle(width / 2, height / 2, width / 3, Path.Direction.CCW);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        canvas.clipPath(path,  Region.Op.DIFFERENCE);
 
-            canvas.clipOutPath(path);
+    }
 
-        }else{
+    private static void drawShadowCircle(Canvas canvas, int width, int height) {
 
-            canvas.clipPath(path);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setShader(new RadialGradient(width / 2, height / 2,
+                (width/3) * 1.1f, Color.BLACK, Color.TRANSPARENT, Shader.TileMode.CLAMP));
 
-        }
+        canvas.drawCircle(width/2, height/2, (width/3) * 1.1f , paint);
 
-        return bitMap;
+    }
+
+    private static void drawCircle(Canvas canvas, int width, int height) {
+
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(Color.WHITE);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(6f);
+        canvas.drawCircle(width/2, height/2, width/3, paint);
+
+    }
+
+    private static void drawBackground(Canvas canvas) {
+
+        canvas.drawColor(0xdd007600);
 
     }
 
